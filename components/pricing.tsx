cd832c7dd306055175c4c820913479f1ef4dc406
Joseph Morrison <pixelatesolutionsllc@gmail.com>
2026-06-21 00:21:26 -0400

    j

--- components/pricing.tsx
+++ components/pricing.tsx
@@ -31,7 +31,7 @@ const plans = [
   },
   {
     name: 'Hourly Build',
-    price: '100',
+    price: '$150',
     cadence: '/hour',
     icon: Clock3,
     accent: 'text-cyan-600 dark:text-cyan-300',
@@ -43,7 +43,7 @@ const plans = [
   },
   {
     name: 'Accelerated Build',
-    price: '$150',
+    price: '$250',
     cadence: '/hour',
     icon: Rocket,
     accent: 'text-amber-600 dark:text-amber-300',
